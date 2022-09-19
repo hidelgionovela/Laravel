0f25ae729e689f649bd82eff55bc4465a5d9042d
Hidélgio Novela <hidelgionovela@gmail.com>
2022-09-19 20:36:36 +0200

    introducao ao estudo de rotas no laravel

--- cursoLaravel/routes/web.php
+++ cursoLaravel/routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ----------- INTRODUCACAO A ROTAS -------------//
+
+// metodo get permite ter acesso aos dados da rota via url
+Route::get('/empresa', function () {
+    return view('site/empresa');
+});
+
+
+//  metodo any Permite qualquer tipo de acesso http (put, delete, get, post); mas nao ee seguro
+Route::any('/acessoTotal', function () {
+    return "Permite qualquer tipo de acesso http";
+});
+
+//  metodo match Permite apenas tipo de acesso http definidos como parametro (put, delete, get, post)
+Route::match(['get','post'],'/definidos', function () {
+    return "Permite acessos http definidos";
+});
+
+
+// passagem de parametros
+
+Route::get('/estudante/{id}/{curso}', function ($nrEstudante, $curso) {
+    return "O numero de estudante ee: $nrEstudante <br>". "O curso dele ee $curso" ;
+});
+
+// Route::get('/estudante/{id}/{curso}', function ($nrEstudante = '', $curso = '') {
+//     return "O numero de estudante ee: $nrEstudante <br>". "O curso dele ee $curso" ;
+// });
+// Podemos atribuir valores default nos parametros para que caso nao seja passados pelo parametros a aplicacao nao de erro 404
+// seguindo o exemplo comentado.
+
+
+// ---------------- REDIRECIONAMENTOS E VIEW------//
