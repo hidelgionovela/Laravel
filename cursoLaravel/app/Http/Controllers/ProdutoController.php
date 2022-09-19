5c4c76c038507fe0a524c4bfc8a91ad7100611af
Hidélgio Novela <hidelgionovela@gmail.com>
2022-09-19 22:26:15 +0200

    introducao aos controler

---
+++ cursoLaravel/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index() {
+        return 'index';
+    }
+    public function show($id) {
+        return "show $id";
+    }
+    public function mostrar($id = '') {
+        return "mostrar $id";
+    }
+}
