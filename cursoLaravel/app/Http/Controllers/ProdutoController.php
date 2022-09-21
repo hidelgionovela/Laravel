67e777d01586fd2a99e3ee76f9fc033e4b22e7f5
Hidélgio Novela <hidelgionovela@gmail.com>
2022-09-21 13:53:33 +0200

    Controller resource

--- cursoLaravel/app/Http/Controllers/ProdutoController.php
+++ cursoLaravel/app/Http/Controllers/ProdutoController.php
@@ -4,15 +4,81 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ProdutoController extends Controller
+class produtoController extends Controller
 {
-    public function index() {
-        return 'index';
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return "Ola Hidelgio";
     }
-    public function show($id) {
-        return "show $id";
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
-    public function mostrar($id = '') {
-        return "mostrar $id";
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
--- cursoLaravel/routes/web.php
+++ cursoLaravel/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\produtoController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProdutoController; // importando o controler apartir do namespace
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,175 +17,13 @@ use App\Http\Controllers\ProdutoController; // importando o controler apartir do
 
 // ---------------- Introducao aos Controlers ---------------------//
 
-Route::get('/', [ProdutoController::class, 'index']
-);
+// ---------------- Controlers Resources ---------------------//
 
-// ---------------- passando parametros aos Controlers ---------------------//
+// criando uma rota para acessar os metodos do produtoController. 
+Route::resource('produtos', produtoController::class);
 
-Route::get('/produto/{id}', [ProdutoController::class, 'show']
-);
-
-// para evitar o 404 caso a url nao contenha o parametro id depois do parametro id temos que colocar uma interogacao
-Route::get('/estudante/{id?}', [ProdutoController::class, 'mostrar']
-);
 
 
 
 // ---------------- Fim do tema Controlers ---------------------//
-
-
-
-
-// ----------------- INTRODUCACAO A ROTAS -------------------//
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-
-// metodo get permite ter acesso aos dados da rota via url
-// Route::get('/empresa', function () {
-//     return view('site/empresa');
-// });
-
-// Route::get('/ujc', function () {
-//     return view('site/ujc');
-// });
-
-
-//  metodo any Permite qualquer tipo de acesso http (put, delete, get, post); mas nao ee seguro
-// Route::any('/acessoTotal', function () {
-//     return "Permite qualquer tipo de acesso http";
-// });
-
-//  metodo match Permite apenas tipo de acesso http definidos como parametro (put, delete, get, post)
-// Route::match(['get', 'post'], '/definidos', function () {
-//     return "Permite acessos http definidos";
-// });
-
-
-// passagem de parametros
-
-// Route::get('/estudante/{id}/{curso}', function ($nrEstudante, $curso) {
-//     return "O numero de estudante ee: $nrEstudante <br>" . "O curso dele ee $curso";
-// });
-
-// Route::get('/estudante/{id}/{curso}', function ($nrEstudante = '', $curso = '') {
-//     return "O numero de estudante ee: $nrEstudante <br>". "O curso dele ee $curso" ;
-// });
-// Podemos atribuir valores default nos parametros para que caso nao seja passados pelo parametros a aplicacao nao de erro 404
-// seguindo o exemplo comentado.
-
-
-// ---------------- REDIRECIONAMENTOS E VIEW------//
-
-// neste caso ao requisitar a rota sobre ele vai nos redirecionar a rota empresa
-// Route::get('/sobre', function () {
-//     return redirect('/empresa');
-// });
-
-// mas podemos tambem fazer o redirecionamento da seguinte maneira sem usar a funcao de callback
-
-// Route::redirect('/about', '/ujc');
-// quando queremos rederizar uma view apenas podemos usar esse modelo 
-// Route::view('/empresa', '/site/empresa');
-
-
-// ---------------- Rotas nomeadas -------------------//
-// O uso de nomes nas rotas e bom ou ee importante pois quando usamos o redirecionamento com o nome da rota por mais que a rota mude com o tempo se o nome ainda for o mesmo nos poderemos ter a acesso a nova rota
-
-// Route::get('/news', function () {
-//     return view('news');
-// })->name('noticias');
-
-// Route::get('/novidades', function () {
-//     return redirect()->route('noticias');
-// });
-
-
-// ------------------- Grupo de Rotas---------------------//
-
-// Route::get('admin/dashbord', function () {
-//     return 'dashbord';
-// });
-
-// Route::get('admin/users', function () {
-//     return 'users';
-// });
-
-// Route::get('admin/clientes', function () {
-//     return 'clientes';
-// });
-
-// temos as rotas acima com uma repeticao "admin" no inicio da rota entao para esses casos nos podemos optimizar o codigo jogando essas rotas num grupo com prefixo admin
-
-// Route::prefix('admin')->group(function () {
-
-//     Route::get('dashbord', function () {
-//         return 'dashbord';
-//     });
-
-//     Route::get('users', function () {
-//         return 'users';
-//     });
-
-//     Route::get('clientes', function () {
-//         return 'clientes';
-//     });
-// });
-
-
-// Mas para alem de arupar pela rota em caso de termos rotas nomeadas podemos agrupar pelos nomes
-
-// Route::name('estudante.')->group(function () {
-
-//     Route::get('estudante/notas', function () {
-//         return 'Notas';
-//     })->name('notas');
-
-//     Route::get('estudante/disciplinas', function () {
-//         return 'Disciplinas';
-//     })->name('disciplinas');
-
-//     Route::get('estudante/faltas', function () {
-//         return 'Faltas';
-//     })->name('faltas');
-// });
-// Teste
-// Route::get('/h', function () {
-//     return redirect()->route('estudante.faltas');
-// });
-
-
-// EE possivel agrupar por name e prefixo ao mesmo tempo tambem
-
-
-// Route::group([
-//     'prefix' => 'estudante',  //informando o prefixo do grupo
-//     'as' => 'estudante.'  //informando o none do grupo && a chave para o name no goup ee "as" 
-// ], function () {
-
-//     Route::get('notas', function () {
-//         return 'Notas';
-//     })->name('notas');
-
-//     Route::get('disciplinas', function () {
-//         return 'Disciplinas';
-//     })->name('disciplinas');
-
-//     Route::get('faltas', function () {
-//         return 'Faltas';
-//     })->name('faltas');
-// });
-// Teste do name
-// Route::get('/h', function () {
-//     return redirect()->route('estudante.faltas');
-// });
-
-
-
-
-
-
 
