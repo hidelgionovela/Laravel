cde7ee8de1f00d02600f85812a949ca66c81b4e5
Hidélgio Novela <hidelgionovela@gmail.com>
2022-11-11 08:20:07 +0200

    colocando o padrão das rotas

--- cursoLaravel/routes/web.php
+++ cursoLaravel/routes/web.php
@@ -22,22 +22,23 @@ use Illuminate\Support\Facades\Route;
 
 // criando uma rota para acessar os metodos do produtoController. 
 Route::resource('produtos', produtoController::class);
-
-// Para listar usamos no laravel geralmente o metodo get e nos controlleres usamos a action index
-
 Route::get('/', function () {
         return view('welcome');
 });
 
-Route::get('/listar', [MachinesController::class, 'index']);
+// Para listar usamos no laravel geralmente o metodo get e nos controlleres usamos a action index
 
+/*Route::get('/listar', [MachinesController::class, 'index']);
 // Para listar usamos no laravel geralmente o metodo get e nos controlleres usamos a action index 
 // Mas podemos usar tambem o metodo show.
-
 Route::get('/visualizar/{id}', [MachinesController::class, 'show']);
 Route::get('/cadastrar', [MachinesController::class, 'create']);
 Route::post('/cadastrar', [MachinesController::class, 'store']);
+*/
 
+// Para trabalhar com link's O padrao das rotas deve ser o seginte:
+Route::resource('machines', MachinesController::class);
+// Entao para acessar metodo index do MachinesController apenas escrevemos machines depois do barra e para acessar outros metodos machines.nomeMetodo.
 
 
 
