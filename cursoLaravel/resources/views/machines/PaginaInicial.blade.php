71a402089d6c1198456f6c330c9c465dfde99874
Hidélgio Novela <hidelgionovela@gmail.com>
2022-11-29 22:46:20 +0200

    Update PaginaInicial.blade.php

--- cursoLaravel/resources/views/machines/PaginaInicial.blade.php
+++ cursoLaravel/resources/views/machines/PaginaInicial.blade.php
@@ -2,31 +2,29 @@
 
 <x-layout title="listarMaquinas">
 
-        <a href="{{route('machines.create')}}">Cadastrar</a>
-        <h1>Listar as Maquinas</h1>
-        @include('components.flash-msg')
+    <a href="{{ route('machines.create') }}">Cadastrar</a>
+    <h1>Listar as Maquinas</h1>
+    @include('components.flash-msg')
 
-        <table style="border: 1px solid black;">
-                <thead>
-                        <tr>
-                                <th>ID_Maquina</th>
-                                <th>Nome_Maquina</th>
-                                <th>Acoes</th>
+    <table style="border: 1px solid black;">
+        <thead>
+            <tr>
+                <th>ID_Maquina</th>
+                <th>Nome_Maquina</th>
+                <th>Acoes</th>
 
-                                
-                              </tr>
-                </thead>
-                <tbody>
-                        @foreach ($machines as $machine)
-                        <tr>
-                        <td>{{$machine->id}}</td>
-                        <td>{{$machine->nome}}</td>
-                        <td><a href="{{route('machines.show', $machine->id)}}">Visualizar</a></td>
-                        </tr>
-                        
-                    @endforeach
-                </tbody>
-                
-              </table>
+
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($machines as $machine)
+                <tr>
+                    <td>{{ $machine->id }}</td>
+                    <td>{{ $machine->nome }}</td>
+                    <td><a href="{{ route('machines.show', $machine->id) }}">Visualizar</a></td>
+                </tr>
+            @endforeach
+        </tbody>
+
+    </table>
 </x-layout>
-
