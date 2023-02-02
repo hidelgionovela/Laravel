8e3af1e48605865b65a9806aa07bfdaf6a54be8b
Hidélgio Novela <hidelgionovela@gmail.com>
2023-02-02 22:52:18 +0200

    Update news.blade.php

--- cursoLaravel/resources/views/news.blade.php
+++ cursoLaravel/resources/views/news.blade.php
@@ -1,1 +1,15 @@
-<h1>News</h1>
+
+
+<form action="" method="post">
+        <center>
+                <h1>Login</h1>
+        <label for="">Username</label>
+        <input style="padding:8px; margin:10px;" type="text" name="username" id="" placeholder="username">
+        <label  for="">Password</label>
+        <input style="padding:8px; margin:10px;" type="password" name="password" id="" placeholder="password"><br>
+        {{-- <button style="padding:8px; margin:10px;">Tente com Google</button> --}}
+        <button style="padding:8px; margin:10px;" type="submit">Submeter</button>
+</center>
+
+<a href="{{ route('google-auth') }}"> Tente com Google</a>
+</form>
